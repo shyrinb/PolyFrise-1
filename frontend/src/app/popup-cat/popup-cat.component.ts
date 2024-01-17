c72291b14bff14d65bbf773e465c1efd5d343b3d
shyrinb <shyrinbelaloui@gmail.com>
2024-01-17 19:04:07 +0100

    maj

--- frontend/src/app/popup-cat/popup-cat.component.ts
+++ frontend/src/app/popup-cat/popup-cat.component.ts
@@ -2,7 +2,6 @@ import { Component, Inject } from '@angular/core';
 import { MessageService } from '../message.service';
 import { v4 as UUID } from 'uuid';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
@@ -33,56 +32,44 @@ export class PopupCatComponent {
     public dialogRef: MatDialogRef<PopupCatComponent>,
     private messageService: MessageService, 
     private http: HttpClient,
-    private router: Router,
-    private fb: FormBuilder ) { {
-      this.formData = this.fb.group({});
-    }}
-
+    private router: Router) {}
+    
+    dates: Dates[] = [];
+    selectedDates: string[] = []; // Utilisation de string pour les identifiants UUID
+    startYear!: Date;
+    endYear!: Date;
     token!: string;
-    formData: FormGroup; // Ajoutez la déclaration de formData
     categories: any;
-    selectedCategories: Category[] = [];
-    selectedDates: string[] = []; 
+    selectedCategories: string = ""; // Utilisation de string pour les identifiants UUID
     selectedCategoryName: string = '';
     selectedCategoryId: string | null = null;
     selectedEventIds: string[] = [];
-    dates: Dates[] = [];
 
   ngOnInit(): void {
-    this.messageService.getData("category", "").subscribe(response => {
-      this.categories = response;
+    this.messageService.getData("category", "").subscribe((response: Category[]) => {
+      this.categories = response.map(item => ({
+        id: item.id,
+        name: item.name,
+        selected: false
+      }));
     });
   }
 
-  onEventChange(selectedEventId: string): void {
-    // Mettez à jour la liste des événements sélectionnés en fonction de l'état de la case à cocher ou du bouton radio
-    const index = this.selectedEventIds.indexOf(selectedEventId);
-    if (index === -1) {
-      this.selectedEventIds.push(selectedEventId);
-    } else {
-      this.selectedEventIds.splice(index, 1);
-    }
-  }
-
-  getSelectedCategories(): Category[] {
-    return this.categories.filter((cat: Category) => cat.selected);
-  }
-
-  private loadDates(): void {
+  loadDates(): void {
     console.log("charge dates");
     // Réinitialiser les dates
     this.dates = [];
-
+  
     // Charger les dates uniquement si une catégorie est sélectionnée
     if (this.selectedCategories.length > 0) {
-      console.log("catégorie selectionne pour afficher les dates", this.selectedCategoryName);
+      console.log("catégorie selectionne pour afficher les dates", this.selectedCategories);
       // Charger les dates depuis le service ou l'API
-      this.messageService.getDataByCategory("databycategory", { categories: this.selectedCategoryName }).subscribe(
+      this.messageService.getDataByCategory("databycategory", { categories: this.selectedCategories }).subscribe(
         (response: Dates[]) => {
           // Sélectionner la table de modèle en fonction de la catégorie
           let date_name: string = '';
 
-          switch (this.selectedCategories[0].name) {
+          switch (this.selectedCategories) {
             case 'avancees':
               date_name = 'date_avancee';
               break;
@@ -119,64 +106,48 @@ export class PopupCatComponent {
             id: item.id,
             nom: item.nom,
             date: item[date_name],
-            selected: this.selectedEventIds.includes(item.id.toString()) // Vérifiez si l'événement est dans la liste des sélectionnés
+            selected: false
           }));
-          console.log("evenement de la catégorie",this.dates);
-         
+          console.log("evenement de la catégorie", this.dates);
+
           // Mettez la logique pour traiter les dates ici
           this.processSelectedDates();
         },
         (error) => {
           console.error('Erreur lors de la récupération des dates :', error);
         }
-      );
-    }
+    );
+  }
   }
 
-  onCategoryChange(selectedCategoryId: string): void {
-    // Décochez toutes les autres catégories lorsqu'une nouvelle est sélectionnée
-    this.categories.forEach((category: Category) => {
-      category.selected = category.id.toString() === selectedCategoryId;
-    });
-  
-    // Réinitialisez la liste des identifiants d'événements sélectionnés
-    this.selectedEventIds = [];
-  
-    // Mettez à jour le modèle avec l'id de la catégorie sélectionnée
-    this.selectedCategoryId = selectedCategoryId;
-    this.selectedCategories = this.getSelectedCategories();
-
-    if (this.selectedCategories.length > 0) {
-      const categoryName = this.selectedCategories[0].name;
-      this.selectedCategoryName = categoryName;
-      console.log("categorie selectionne", this.selectedCategoryName);
-
-      // Chargez les dates
-      this.loadDates();
-
-      console.log("affiche les colonnes");
-
-    }
-  }
 
   private processSelectedDates() {
     // Mettez la logique pour traiter les dates ici
-    this.selectedEventIds = this.dates.filter(date => date.selected).map(date => date.id.toString());
-  }
+    for (const date of this.dates) {
+      if (date.selected) {
+        this.selectedCategories = date.id.toString();
+      }
+    }
+  } 
   
 
-  onInput(event: any, column: string): void {
-    // Ajoutez la logique nécessaire pour gérer les événements d'entrée ici
-    // Vous pouvez utiliser event.target.value pour obtenir la valeur de l'entrée
-    const value = event.target.value;
-
-    // Faites quelque chose avec la valeur, par exemple, mettez à jour le formulaire
-    this.formData.get(column)?.setValue(value);
-  }
-
   emitData() {
+    const selectedNomEvents = this.selectedDates.map(selectedDateId => {
+      const selectedDate = this.dates.find(date => date.id === selectedDateId);
+      return selectedDate ? selectedDate.nom : ''; // Retourne le nom si trouvé, sinon une chaîne vide
+    });
+
+    const selectedDates = this.selectedDates.map(selectedDateId => {
+      const selectedDate = this.dates.find(date => date.id === selectedDateId);
+      return selectedDate ? selectedDate.date : ''; // Retourne la date si trouvée, sinon une chaîne vide
+    });
+
     console.log("this.selectedEventIds",this.selectedEventIds)
-    this.dialogRef.close({ categories: this.selectedCategoryName, event:this.selectedEventIds});
+    this.dialogRef.close({ 
+      categories: this.selectedCategories, 
+      nom_event:selectedNomEvents.join(', '),
+      date:selectedDates.join(', '),
+      date_id:this.selectedDates });
   }
 
 }
